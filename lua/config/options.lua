08f6fda159cb627f3aa9deb71a5f45e3ef8873ed
Blaj <blaj.kontakt@gmail.com>
2024-08-28 22:50:13 +0200

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,37 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = false
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2 -- spaces for indend width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+opt.cursorline = true
+
+-- themes settings
+opt.termguicolors = true
+opt.background = "dark" -- themes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so taht text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal windows to the bottom
+
+opt.whichwrap = "<>,hl[]"
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  "xiantang/darcula-dark.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme darcula-dark")
+  end
+}
